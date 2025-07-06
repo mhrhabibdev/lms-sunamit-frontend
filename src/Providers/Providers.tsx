587e2fce5f67aa02b04a697d4903c937490203bb
MHR HABIB <mhrhabib2004@gmail.com>
2025-07-06 22:52:35 +0600

    refactor: wrap Providers component with Redux Provider for state management

--- src/Providers/Providers.tsx
+++ src/Providers/Providers.tsx
@@ -1,15 +1,18 @@
 "use client";
 
+import store from "@/redux/store";
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
+import { Provider } from "react-redux";
 
 export default function Providers({ children }: { children: ReactNode }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-
-     <Toaster position="top-center" richColors />
-      {children}
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Toaster position="top-center" richColors />
+        {children}
+      </ThemeProvider>
+    </Provider>
   );
 }
