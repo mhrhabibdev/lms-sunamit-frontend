9a5a0c2b57680d5894e0f775b5e4085c8998b78d
MHR HABIB <mhrhabib2004@gmail.com>
2025-07-26 21:27:31 +0600

    add:loading page

---
+++ src/app/loading.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import "@/styles/loader.css"; 
+
+const LoadingPage = () => {
+  return (
+    <div className="fixed inset-0 z-[9999] flex items-center justify-center bg-black/30 backdrop-blur-sm">
+      <div className="loader">
+        <div className="box box-1">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
+        </div>
+        <div className="box box-2">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
+        </div>
+        <div className="box box-3">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
+        </div>
+        <div className="box box-4">
+          <div className="side-left"></div>
+          <div className="side-right"></div>
+          <div className="side-top"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
