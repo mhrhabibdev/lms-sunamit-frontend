7ab35f621b05ab3ff5e2d7f3fb341f3d1a5188c9
MHR HABIB <mhrhabib2004@gmail.com>
2025-07-26 21:24:09 +0600

    up:provaiders

--- src/Providers/Providers.tsx
+++ src/Providers/Providers.tsx
@@ -1,18 +1,23 @@
 "use client";
 
-import store from "@/redux/store";
+import store, { persistor } from "@/redux/store";
 import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 import { Toaster } from "sonner";
 import { Provider } from "react-redux";
-
+import { SessionProvider } from "next-auth/react";
+import { PersistGate } from "redux-persist/integration/react";
 export default function Providers({ children }: { children: ReactNode }) {
   return (
     <Provider store={store}>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <Toaster position="top-center" richColors />
-        {children}
-      </ThemeProvider>
+       <PersistGate loading={null} persistor={persistor}>
+      <SessionProvider>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Toaster position="top-center" richColors />
+          {children}
+        </ThemeProvider>
+      </SessionProvider>
+      </PersistGate>
     </Provider>
   );
 }
