8552c5112896633b76f3b6750b7649efa3aee2e3
MHR HABIB <mhrhabib2004@gmail.com>
2025-08-26 21:35:52 +0600

    feat: add Textarea component for enhanced text input functionality feat: define CourseInfoType for structured course data management

---
+++ src/type/index.ts
@@ -0,0 +1,10 @@
+export type CourseInfoType = {
+  name: string;
+  description: string;
+  price: string;
+  estimatedPrice: string;
+  tags: string;
+  level: string;
+  demoUrl: string;
+  thumbnail: string;
+};
